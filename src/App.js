05b310eba0bb8009d7a1fcea1bbc6e21f2fc2365
Rementeria <p.gonzalez@outlook.com>
2020-09-23 22:20:54 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const product = {
+    name: 'Apple',
+    quantity: 1
+    };
+
+  const {
+    quantity,
+    ...newProduct
+    } = product;
+
+  const log = () =>{
+    console.log(product);
+    console.log(newProduct);
+  }
+  //
+
   return (
     <div className="App">
       <h1>Titulo</h1>
+      <button onClick={() => log()}>console.log</button>
     </div>
   );
 }
